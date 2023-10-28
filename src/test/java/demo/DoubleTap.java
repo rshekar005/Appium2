65d2037737a4d5066333b47e3ff6e9e7ed6208a9
rshekar005 <rshekar005@gmail.com>
2023-10-28 13:01:12 +0530

    Added code for double tap

---
+++ src/test/java/demo/DoubleTap.java
@@ -0,0 +1,54 @@
+package demo;
+
+import io.appium.java_client.AppiumBy;
+import io.appium.java_client.android.AndroidDriver;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.Point;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.interactions.Pause;
+import org.openqa.selenium.interactions.PointerInput;
+import org.openqa.selenium.interactions.Sequence;
+
+import java.net.MalformedURLException;
+import java.time.Duration;
+import java.util.Collections;
+
+public class DoubleTap extends BaseTest {
+
+    @Test
+    public void openMenuUsingDoubleTap() throws MalformedURLException, InterruptedException {
+        BaseTest baseTest = new BaseTest();
+        baseTest.launchAndroid();
+        WebElement openMenu = androidDriver.findElement(AppiumBy.accessibilityId("open menu"));
+        doubleTap(openMenu, androidDriver);
+
+    }
+
+    public void doubleTap(WebElement element, AndroidDriver driver) throws MalformedURLException, InterruptedException {
+        Point location = element.getLocation();
+        Dimension dimension = element.getSize();
+        Point centerOfElement = getCenterOfElement(location, dimension);
+        PointerInput finger1 = new PointerInput(PointerInput.Kind.TOUCH, "finger1");
+        Sequence sequence =
+                new Sequence(finger1, 1)
+                        .addAction(finger1.createPointerMove(Duration.ZERO, PointerInput.Origin.viewport(), centerOfElement))
+                        .addAction(finger1.createPointerDown(PointerInput.MouseButton.LEFT.asArg()))
+                        .addAction(new Pause(finger1, Duration.ofMillis(100)))
+                        .addAction(finger1.createPointerUp(PointerInput.MouseButton.LEFT.asArg()))
+                        .addAction(new Pause(finger1, Duration.ofMillis(100)))
+                        .addAction(finger1.createPointerDown(PointerInput.MouseButton.LEFT.asArg()))
+                        .addAction(finger1.createPointerUp(PointerInput.MouseButton.LEFT.asArg()))
+                        .addAction(new Pause(finger1, Duration.ofMillis(100)));
+
+        androidDriver.perform(Collections.singletonList(sequence));
+
+    }
+
+    // This method is used to get the center of an element.
+    private Point getCenterOfElement(Point location, Dimension size) {
+        return new Point(location.getX() + size.getWidth() / 2,
+                location.getY() + size.getHeight() / 2);
+    }
+}
